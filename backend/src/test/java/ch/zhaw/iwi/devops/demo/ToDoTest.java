6b76351dc786cea1df51c8b6cbafe1e80830104f
sumoh001 <umoh.simon@yahoo.de>
2024-05-20 17:17:36 +0200

    clean house

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
@@ -1,6 +1,6 @@
 package ch.zhaw.iwi.devops.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
