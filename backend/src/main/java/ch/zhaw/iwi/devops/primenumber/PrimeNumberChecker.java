961001fdcef58051db686151bcfa17508631b69b
sumoh001 <umoh.simon@yahoo.de>
2024-05-20 17:40:23 +0200

    Production PrimeNumberChecker

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/primenumber/PrimeNumberChecker.java
@@ -0,0 +1,16 @@
+package ch.zhaw.iwi.devops.primenumber;
+
+public class PrimeNumberChecker {
+
+    public boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
